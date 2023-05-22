5e486d626edf328972d17e96ff29205d3ba312f5
Michele Palamidessi <michele.palamidessi@protonmail.com>
2023-05-22 09:37:44 +0200

    feat: support object element in datasource

--- test/basic.test.js
+++ test/basic.test.js
@@ -46,3 +46,51 @@ tap.test(
     tap.end()
   }
 )
+
+tap.test(
+  'It should throw in case element of the datasource are not an array of two elements',
+  (tap) => {
+    tap.throws(
+      () => {
+        tap.tests(
+          'Test with object datasource',
+          () => [
+            [1, 2],
+            [1, 2, 3],
+          ],
+          (tap) => {
+            tap.fail('Should not reach this point')
+            tap.end()
+          }
+        )
+      },
+      {
+        message: 'Datasource array element #1 must have exactly 2 element',
+      }
+    )
+    tap.end()
+  }
+)
+
+tap.test(
+  'It should throw in case element of the datasource are not an object with input and expected fields',
+  (tap) => {
+    tap.throws(
+      () => {
+        tap.tests(
+          'Test with object datasource',
+          () => [{ input: 1, expected: 2 }, { input: 1 }],
+          (tap) => {
+            tap.fail('Should not reach this point')
+            tap.end()
+          }
+        )
+      },
+      {
+        message:
+          'Datasource object element #1 does not have input and/or expected field',
+      }
+    )
+    tap.end()
+  }
+)
--- test/tapDataProvider.test.js
+++ test/tapDataProvider.test.js
@@ -41,14 +41,14 @@ tap.test(
   'It should expose a function which accept function source',
   async (tap) => {
     const dataSource = () => [
-      [1, 2],
-      [3, 4],
+      { input: 1, expected: 2 },
+      { input: 3, expected: 4 },
     ]
     const source = dataSource()
     tap.tests(
       'This is a test name',
       dataSource,
-      (tap, input, output, index) => {
+      (tap, input, expected, index) => {
         tap.type(tap, Test, 'First params should be of type tap.Test')
         tap.equal(
           tap.name,
@@ -58,13 +58,13 @@ tap.test(
         )
         tap.equal(
           input,
-          source[index][0],
+          source[index].input,
           'Input should be the first element of the current source'
         )
         tap.equal(
-          output,
-          source[index][1],
-          'Output should be the second element of the current source'
+          expected,
+          source[index].expected,
+          'Expected should be the second element of the current source'
         )
         tap.end()
       }
